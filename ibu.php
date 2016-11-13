e3a513e266ad9ccfd193df038f860a921baacb5f
Matt Startin <m.j.startin@googlemail.com>
2016-11-13 14:51:16 +0000

    Spins round actvities and calculates totals to put onto screen

--- ibu.php
+++ ibu.php
@@ -1,25 +1,44 @@
 <?php
     echo "STRAVA DATA VIEWER";
 
+    // includes
     include('config.php');
-    
-	// create curl resource 
+
+	// curl strava 
     $ch = curl_init(); 
-    
-    // curl strava 
     curl_setopt($ch, CURLOPT_URL, $url); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-    // $output contains the output string 
-    $output = curl_exec($ch); 
-
-    // close curl resource to free up system resources 
+    $rawData = curl_exec($ch); 
     curl_close($ch); 
 
 
-    // spew onto page
-	echo "<br />start<br/>";
-   	echo $output;	
-	echo "<br />end";
+    // loop round data to get information
+	$activities = json_decode($rawData, true);
+    
+    $totalDistance = 0;
+    $totalTime = 0;
+    $totalClimb = 0;
+    $activityCount = 0;
+    $maxSpeed = 0;
+    foreach($activities as $activity) {
+        $activityCount += 1;
+        //echo "<br />" ;
+        //echo $activity["name"] . " - " . $activity["distance"] / 1603.34 . "|" . $activity["moving_time"] / 3600 . "|" . $activity["total_elevation_gain"] . "|" . $activity["average_speed"]/ 0.44704  . "|" . $activity["max_speed"];
+        $totalDistance += $activity["distance"];
+        $totalTime += $activity["moving_time"];
+        $totalClimb += $activity["total_elevation_gain"];
+        if ($activity["max_speed"] > $maxSpeed)
+            $maxSpeed = $activity["max_speed"];
+
+    }
+    $averageSpeed = $totalDistance / $totalTime;
+    echo "<br />" ;
+    echo "<br />" ;
+    echo "<br />" ;
+    echo "TOTAL DISTANCE: " . $totalDistance / 1609.34 ." miles<br />";
+    echo "TOTAL TIME    : " . $totalTime / 3600 ."hours <br />";
+    echo "AVERAGE SPEED : " . $averageSpeed / 0.44704 ."mph<br />";
+    echo "MAX SPEED     : " . $maxSpeed  ."mph<br />";
+    echo "TOTAL CLIMB   : " . $totalClimb . "metres<br />";
         
 ?>
